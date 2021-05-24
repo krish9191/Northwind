40743a06f69226461dfa151bbaba82eb86fc4ac1
krishna <contact.krishnatiwari@gmail.com>
2021-05-24 18:50:28 +0200

    add url in run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
-from resources.list_employee_resource import EmployeeInfo
+from resources.list_employees_resource import EmployeeInfo
+from resources.list_customers_resource import CustomerInfo
 from extension import db
 
 app = Flask(__name__)
@@ -8,7 +9,8 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/northwind'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-api.add_resource(EmployeeInfo, '/employee/<int:id>')
+api.add_resource(EmployeeInfo, '/employees/<id>')
+api.add_resource(CustomerInfo, '/customers/<id>')
 
 if __name__ == '__main__':
     db.init_app(app)
